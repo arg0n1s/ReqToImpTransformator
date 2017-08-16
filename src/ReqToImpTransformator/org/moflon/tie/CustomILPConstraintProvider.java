e43885bd2c1850cf074d0f0947b389991cf33ae9
Nsteel <nicoacero94@gmail.com>
2017-08-16 22:46:59 +0200

    Refactoring + MaxSpeed Constraint

--- src/ReqToImpTransformator/org/moflon/tie/CustomILPConstraintProvider.java
+++ src/ReqToImpTransformator/org/moflon/tie/CustomILPConstraintProvider.java
@@ -1,6 +1,5 @@
 package ReqToImpTransformator.org.moflon.tie;
 
-import java.nio.file.attribute.UserDefinedFileAttributeView;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -11,44 +10,78 @@ import org.moflon.tgg.algorithm.ccutils.UserDefinedILPConstraintProvider;
 import org.moflon.tgg.algorithm.datastructures.ConsistencyCheckPrecedenceGraph;
 import org.moflon.tgg.runtime.CCMatch;
 
+import implementation.Cable;
 import implementation.Server;
+import requirements.Provider;
 
 public class CustomILPConstraintProvider implements UserDefinedILPConstraintProvider {
 
 	@Override
 	public Collection<UserDefinedILPConstraint> getUserDefinedConstraints(ConsistencyCheckPrecedenceGraph protocol) {
-		Map<Server, Collection<Integer>> serverIdMap = new HashMap<>();
-		Map<String, HashMap<Integer, Double>> idToCoefficientMap = new HashMap<>();
-		//Map<String, HashMap<Integer, Double>> idToCoefficientMap_MTBF = new HashMap<>();
-		double meanTimeBetweenFailures = 0.0;
+		Map<CCMatch, Integer> matchesMap = new HashMap<>();
 		for(CCMatch m : protocol.getMatches()) {
 			if(m.getRuleName().equals("ReqProviderToServerRule")) {
-				Server s = (Server)m.getTargetMatch().getNodeMappings().get("implDevice");
-				Collection<Integer> ids = serverIdMap.getOrDefault(s.getName(), new ArrayList<>());
-				ids.add(protocol.matchToInt(m));
-				serverIdMap.put(s, ids);
-				
-				HashMap<Integer, Double> coefficients = idToCoefficientMap.getOrDefault(s.getName(), new HashMap<>());
-				//HashMap<Integer, Double> coefficients_MTBF = idToCoefficientMap_MTBF.getOrDefault(s.getName(), new HashMap<>());
-				
-				meanTimeBetweenFailures = s.getMTBF().doubleValue();
-				System.out.println(meanTimeBetweenFailures);
-				coefficients.put(protocol.matchToInt(m), 1.0);
-				//coefficients_MTBF.put(protocol.matchToInt(m), meanTimeBetweenFailures);
-				
-				idToCoefficientMap.put(s.getName(), coefficients);
-				//idToCoefficientMap_MTBF.put(s.getName(), coefficients_MTBF);
-				
+				matchesMap.put(m, protocol.matchToInt(m));
 			}
 		}
 		
 		Collection<UserDefinedILPConstraint> results = new ArrayList<>();
-		for(Server s : serverIdMap.keySet()) {
-			//results.add(new UserDefinedILPConstraint(idToCoefficientMap_MTBF.get(s.getName()), "<=", 400.0));
-			results.add(new UserDefinedILPConstraint(idToCoefficientMap.get(s.getName()), "<=", s.getMaxSlots().doubleValue()));
-		}
+		
+		results = maxSlotsConstraint(protocol, matchesMap, results);
+		results = speedConstraint(protocol, matchesMap, results);
+		
 		return results;
 	}
 	
+	private Collection<UserDefinedILPConstraint> maxSlotsConstraint(ConsistencyCheckPrecedenceGraph protocol, Map<CCMatch, Integer> matchesMap, Collection<UserDefinedILPConstraint> results) {
+		
+		Map<Server, HashMap<Integer, Double>> idToCoefficientMap = new HashMap<>();
+		
+		for (CCMatch m : matchesMap.keySet()) {
+			Server s = (Server)m.getTargetMatch().getNodeMappings().get("implDevice");
+			
+			HashMap<Integer, Double> coefficients = idToCoefficientMap.getOrDefault(s, new HashMap<>());
+			coefficients.put(matchesMap.get(m), 1.0);
+			
+			idToCoefficientMap.put(s, coefficients);
+		}
+		
+		for(Server s : idToCoefficientMap.keySet()) {
+			results.add(new UserDefinedILPConstraint(idToCoefficientMap.get(s), "<=", s.getMaxSlots().doubleValue()));
+		}
 
+		return results;
+	}
+	
+	private Collection<UserDefinedILPConstraint> speedConstraint(ConsistencyCheckPrecedenceGraph protocol, Map<CCMatch, Integer> matchesMap, Collection<UserDefinedILPConstraint> results) {
+		Map<String, HashMap<Integer, Double>> idToCoefficientMap = new HashMap<>();
+		Map<String, Double> serverSpeedMap = new HashMap<>();
+		
+		for (CCMatch m : matchesMap.keySet()) {
+			Server s = (Server)m.getTargetMatch().getNodeMappings().get("implDevice");
+			Provider p = (Provider)m.getSourceMatch().getNodeMappings().get("reqAgent");
+			
+			if(!serverSpeedMap.containsKey(s.getName())) {
+				double serverSpeed = 0.0;
+				for (Cable c : s.getOutgoing()) {
+					serverSpeed += c.getSpeed().doubleValue();
+				}
+				// In order to simplify things, we assume that the maximum server speed (actually we are talking about throughput) 
+				// is equals to the sum over the speed of its outgoing cables.
+				serverSpeedMap.put(s.getName(), serverSpeed);
+			}
+			
+			HashMap<Integer, Double> coefficients = idToCoefficientMap.getOrDefault(s.getName(), new HashMap<>());
+			coefficients.put(matchesMap.get(m), p.getSpeed().doubleValue());
+			
+			idToCoefficientMap.put(s.getName(), coefficients);
+		}
+		
+		for(String serverName : idToCoefficientMap.keySet()) {
+			results.add(new UserDefinedILPConstraint(idToCoefficientMap.get(serverName), "<=", serverSpeedMap.get(serverName)));
+		}
+
+		return results;
+	}
+	
 }
